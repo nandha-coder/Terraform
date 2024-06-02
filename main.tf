4af8987166055843eb14c66493d7c3f974384f88
Nandhakumar Madheshwaran <63140143+nandha-coder@users.noreply.github.com>
2024-06-02 14:26:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+
+## VPC Creation and make it as a Default Tenancy
+
 resource "aws_vpc" "mainvpc" {
   cidr_block       = "10.244.0.0/16"
   instance_tenancy = "default"
@@ -6,6 +9,9 @@ resource "aws_vpc" "mainvpc" {
     Name = "mainvpc"
   }
 }
+
+## Subnet 1
+
 resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.mainvpc.id
   cidr_block = "10.244.1.0/24"
@@ -15,6 +21,8 @@ resource "aws_subnet" "private_subnet_1" {
     Name = "private_subnet_1"
   }
 }
+
+## Subnet 2
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id     = aws_vpc.mainvpc.id
@@ -26,6 +34,8 @@ resource "aws_subnet" "private_subnet_2" {
   }
 }
 
+## Subnet 3
+
 resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.mainvpc.id
   cidr_block = "10.244.128.0/24"
@@ -35,6 +45,8 @@ resource "aws_subnet" "public_subnet_1" {
     Name = "public_subnet_1"
   }
 }
+
+## Subnet 4
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id     = aws_vpc.mainvpc.id
@@ -46,18 +58,19 @@ resource "aws_subnet" "public_subnet_2" {
   }
 }
 
+## Security Group
 
 resource "aws_security_group" "main_sg" {
   name        = "main_sg"
   vpc_id      = aws_vpc.mainvpc.id
-
-
+## Ingress Rules
   ingress {
     from_port       = 0
     to_port         = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+## Egress Rules
   egress {
     from_port       = 0
     to_port         = 0
@@ -70,8 +83,6 @@ resource "aws_security_group" "main_sg" {
   }
 }
 
-
-
 resource "aws_internet_gateway" "maininternetgateway" {
   vpc_id = aws_vpc.mainvpc.id
 
@@ -82,23 +93,6 @@ resource "aws_internet_gateway" "maininternetgateway" {
 
 
 #========== Route Table =========
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.mainvpc.id
-
-  route {
-    cidr_block = "10.244.0.0/16"
-    gateway_id = "local"
-  }
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.maininternetgateway.id
-  }
-
-
-  tags = {
-    Name = "private_route_table"
-  }
-}
 
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.mainvpc.id
@@ -106,7 +100,7 @@ resource "aws_route_table" "public_route_table" {
   route {
     cidr_block = "10.244.0.0/16"
     gateway_id = "local"
-	}
+        }
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.maininternetgateway.id
@@ -121,12 +115,12 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table_association" "rta_private1" {
   subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "rta_private2" {
   subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "rta_public_1" {
@@ -143,10 +137,9 @@ resource "aws_instance" "webserver-1" {
   ami           = "ami-00beae93a2d981137"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet_1.id
-  key_name	= "us-east-1" 
   security_groups = [aws_security_group.main_sg.id]
   associate_public_ip_address = true
-  user_data       = base64encode(file("userdata1.sh"))
+  user_data       = base64encode(file("svr1.sh"))
 
   tags = {
     Name = "webserver-1"
@@ -157,10 +150,9 @@ resource "aws_instance" "webserver-2" {
   ami           = "ami-00beae93a2d981137"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet_2.id
-  key_name	= "us-east-1"
   security_groups = [aws_security_group.main_sg.id]
   associate_public_ip_address = true
-  user_data       = base64encode(file("userdata2.sh"))
+  user_data       = base64encode(file("svr2.sh"))
 
   tags = {
     Name = "webserver-2"
@@ -180,7 +172,7 @@ resource "aws_lb" "myloadbalancer" {
 
   tags = {
     Environment = "mainLB"
-	Name 		= "mainLB"
+        Name            = "mainLB"
   }
 }
 
@@ -188,10 +180,7 @@ output Loadbalancername {
 value = aws_lb.myloadbalancer.dns_name
 }
 
-
-
 ##  Target Group
-
 
 resource "aws_lb_target_group" "targetgroup1" {
   name     = "targetgroup1"
@@ -219,7 +208,7 @@ resource "aws_lb_target_group_attachment" "attachment_2" {
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.myloadbalancer.arn
-  port              = "8080"
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
