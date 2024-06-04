6b701970119fd615eeff5bd69a72485ddf8a5ea0
Nandhakumar Madheshwaran <63140143+nandha-coder@users.noreply.github.com>
2024-06-04 15:05:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,45 +1,15 @@
-## Central Location for Few IMportant Variables.
+## Central Location for Variable Defention.
 
-## This Variables should be evaluated before applying the terraform project.
-
-variable "region" {
-default = "us-east-1"
-}
-variable "vpc_cidr_range" {
-default = "10.244.0.0/16"
-}
-variable "private_subnet_1" {
-default = "10.244.1.0/24"
-}
-variable "availability_zone_pvt_1" {
-default ="us-east-1a"
-}
-
-variable "private_subnet_2" {
-default = "10.244.2.0/24"
-}
-variable "availability_zone_pvt_2" {
-default ="us-east-1b"
-}
-
-variable "public_subnet_1" {
-default = "10.244.128.0/24"
-}
-variable "availability_zone_pub_1" {
-default ="us-east-1a"
-}
-variable "public_subnet_2" {
-default = "10.244.127.0/24"
-}
-variable "availability_zone_pub_2" {
-default ="us-east-1b"
-}
-variable "instance_type" {
-default = "t2.micro"
-}
-variable "instance_ami_id" {
-default = "ami-00beae93a2d981137"
-}
-variable "instance_public_ip" {
-default = "true"
-}
+variable "region" {}
+variable "vpc_cidr_range" {}
+variable "private_subnet_1" {}
+variable "availability_zone_pvt_1" {}
+variable "private_subnet_2" {}
+variable "availability_zone_pvt_2" {}
+variable "public_subnet_1" {}
+variable "availability_zone_pub_1" {}
+variable "public_subnet_2" {}
+variable "availability_zone_pub_2" {}
+variable "instance_type" {}
+variable "instance_ami_id" {}
+variable "instance_public_ip" {}
